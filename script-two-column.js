d9795333439a0fef75a668789eaa833984dd0208
KANNURU SRINADH <srinadh.3914@gmail.com>
2026-04-22 13:48:21 +0530

    S
    S

--- script-two-column.js
+++ script-two-column.js
@@ -8,7 +8,7 @@ function updateDateTime() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    const timeString = `${hours} : ${minutes} : ${seconds}`;
+    const timeString = `${hours}:${minutes}:${seconds}`;
     
     // Format date (DAY, MONTH DATE, YEAR)
     const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
